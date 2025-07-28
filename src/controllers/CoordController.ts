d2325ca7c95efd65808e98ea63a5bdf37fb987a2
Pedro Cayô <pedro_h_t_cayo@hotmail.com>
2025-07-28 17:19:29 -0300

    fix: Validations before catch Error

--- src/controllers/CoordController.ts
+++ src/controllers/CoordController.ts
@@ -6,19 +6,10 @@ export default class CoordController {
     static coord: ExpressType = async (req: Request, res: Response) => {
     try {
           const info = req.body
-
-          if (!Array.isArray(info)) {
+        // Validação se está enviando um Array de pontos
+        if (!Array.isArray(info)) {
               return res.status(400).json({ message: 'Esperado um array de pontos.' })
           }
-          for (const coord of info) {
-              if (!coord.x || !coord.y) {
-                return res.status(422).json({ message: 'Todos os pontos devem possuir as coordenadas x e y.' })
-              }
-            }
-          const savedCoords = await Coord.insertMany(info)
-          res.status(201).json(savedCoords)
-    }catch(error) {
-        const info = req.body
         // Validações de Valores Númericos
         for(const coord of info){
             if (typeof coord.x !== 'number' || isNaN(coord.x)) {
@@ -29,16 +20,29 @@ export default class CoordController {
               }
             if (typeof coord.id !== 'number' || isNaN(coord.id)) {
                 res.status(422).json({ message: 'O valor do id deve ser númerico.' })
-              } 
+              }
+            if (!coord.x || !coord.y) {
+                return res.status(422).json({ message: 'Todos os pontos devem possuir as coordenadas x e y.' })
+              }
+        // Validações de pares de coordenadas ou Id's iguais 
             const equalPairs = info.map((coord: any) => `${coord.x}, ${coord.y}`)
-            const duplicates = equalPairs.filter((pair: string, index: number) => equalPairs.indexOf(pair) !== index)
-            if (duplicates.length > 0) {    
+            const duplicatePairs = equalPairs.filter((pair: string, index: number) => equalPairs.indexOf(pair) !== index)
+            if (duplicatePairs.length > 0) {    
                 return res.status(422).json({
-                message: `Coordenadas duplicadas detectadas: (${duplicates[0]})`
+                message: `Coordenadas duplicadas detectadas: (${duplicatePairs[0]})`
+                })
+            }
+            const equalId = info.map((coord: any) => coord.id)
+            const duplicateId = equalId.filter((id: any, index: number) => equalId.indexOf(id) !== index)
+            if (duplicateId.length > 0) {    
+                return res.status(422).json({
+                message: `Coordenadas duplicadas detectadas: (${duplicateId[0]})`
                 })
             }
         }
-        
+          const savedCoords = await Coord.insertMany(info)
+          res.status(201).json(savedCoords)
+    }catch(error) {
     console.error(error)
       res.status(500).json({ message: 'Erro ao salvar coordenadas.' })
     }
