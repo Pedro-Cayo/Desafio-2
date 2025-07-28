90d7e23655edebd592aacfe571ad8ebc22b61cd6
Pedro Cayô <pedro_h_t_cayo@hotmail.com>
2025-07-28 16:47:41 -0300

    feat: More Validations

--- src/controllers/CoordController.ts
+++ src/controllers/CoordController.ts
@@ -6,6 +6,7 @@ export default class CoordController {
     static coord: ExpressType = async (req: Request, res: Response) => {
     try {
           const info = req.body
+
           if (!Array.isArray(info)) {
               return res.status(400).json({ message: 'Esperado um array de pontos.' })
           }
@@ -17,8 +18,29 @@ export default class CoordController {
           const savedCoords = await Coord.insertMany(info)
           res.status(201).json(savedCoords)
     }catch(error) {
-        console.error(error)
-        res.status(500).json({message: 'Erro ao salvar coordenadas'})
+        const info = req.body
+        // Validações de Valores Númericos
+        for(const coord of info){
+            if (typeof coord.x !== 'number' || isNaN(coord.x)) {
+                res.status(422).json({ message: 'O valor de x deve ser númerico.'})
+              }
+            if (typeof coord.y !== 'number' || isNaN(coord.y)) {
+                res.status(422).json({ message: 'O valor de y deve ser númerico.' })
+              }
+            if (typeof coord.id !== 'number' || isNaN(coord.id)) {
+                res.status(422).json({ message: 'O valor do id deve ser númerico.' })
+              } 
+            const equalPairs = info.map((coord: any) => `${coord.x}, ${coord.y}`)
+            const duplicates = equalPairs.filter((pair: string, index: number) => equalPairs.indexOf(pair) !== index)
+            if (duplicates.length > 0) {    
+                return res.status(422).json({
+                message: `Coordenadas duplicadas detectadas: (${duplicates[0]})`
+                })
+            }
+        }
+        
+    console.error(error)
+      res.status(500).json({ message: 'Erro ao salvar coordenadas.' })
     }
   }
   }   
