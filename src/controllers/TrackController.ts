bfc4dee21ab0122f27e365a678a91f5663792543
Pedro Cayô <pedro_h_t_cayo@hotmail.com>
2025-07-29 19:05:02 -0300

    feat: req3.d complete - get/rota/:id

--- src/controllers/TrackController.ts
+++ src/controllers/TrackController.ts
@@ -1,27 +1,18 @@
 import {Request, Response } from 'express'
 import Coord from '../models/Coord'
 import { ExpressType } from '../types/types'
-import { coordDistances } from '../utils/calcFunctions'
-import { Locations } from '../types/types'
+import {optimalTrack} from '../utils/calcFunctions'
 
 export default class TrackController {
     static bestTrackById: ExpressType = async (req: Request, res: Response) => {
         try {
-            const info: Locations[] = req.body
             const id = req.params.id
             const coord = await Coord.findById(id)
-           
             if (!coord || !coord.Coordinates || coord.Coordinates.length < 2) {
-                return res.status(404).json({ message: 'A coordenada não existe ou não há outra para comparação.' })
+                return res.status(404).json({ message: 'A coordenada não existe ou não há mais nenhuma coordenada para comparação.' })
             }
-            const origin = coord.Coordinates[0]
-            const results = coord.Coordinates.slice(1).map(nextId =>({
-                originId: origin.id,
-                nextId: nextId.id,
-                distance: coordDistances(origin, nextId)
-            }))
-            return res.status(200).json({
-            message: `Distâncias calculadas a partir do ponto ID ${origin.id}`, results})
+            const bestTrack = optimalTrack(coord.Coordinates);
+            return res.status(200).json(bestTrack);
             
         } catch (error) {
             console.error(error)
